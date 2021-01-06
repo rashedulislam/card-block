218adfe1e82026ca2424bf7259e828ecd0ed784d
Md Rashedul Islam <rashedulislam.ruet@gmail.com>
2021-01-06 23:56:06 +0600

    cleaned up default template

--- plugin.php
+++ plugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: gutenberg-card-block — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: gutenberg-card-block — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Gutenberg Card Block
+ * Plugin URI: https://rashedul.co/card-block/
+ * Description: Creating Plugin for learning
+ * Author: Md Rashedul Islam
+ * Author URI: https://rashedul.co
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package CGB
+ * @package RACardBlock
  */
 
 // Exit if accessed directly.
